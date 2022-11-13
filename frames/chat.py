f22a6618592a75c13c57af8c5b4e24624c7f9e77
Manuel Šarić <manujelko@gmail.com>
2022-11-13 16:56:00 +0100

    Create scrollable frame

--- frames/chat.py
+++ frames/chat.py
@@ -1,9 +1,8 @@
-import datetime
-import tkinter as tk
 from tkinter import ttk
-from PIL import Image, ImageTk
 
 import requests
+
+from .message_window import MessageWindow
 
 messages = [{"message": "Hello, world", "date": 15498487}]
 message_labels = []
@@ -16,8 +15,8 @@ class Chat(ttk.Frame):
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
 
-        self.messages_frame = ttk.Frame(self)
-        self.messages_frame.grid(row=0, column=0, sticky="NSEW", pady=5)
+        self.message_window = MessageWindow(self)
+        self.message_window.grid(row=0, column=0, sticky="NSEW", pady=5)
 
         input_frame = ttk.Frame(self, padding=10)
         input_frame.grid(row=1, column=0, sticky="EW")
@@ -28,60 +27,10 @@ class Chat(ttk.Frame):
             command=self.get_messages,
         )
         message_fetch.pack()
-        self.update_message_widgets()
+        self.message_window.update_message_widgets(messages, message_labels)
     
     def get_messages(self):
         global messages
         messages = requests.get("http://167.99.63.70/messages").json()
-        self.update_message_widgets()
+        self.message_window.update_message_widgets(messages, message_labels)
     
-    def update_message_widgets(self):
-        existing_labels = [
-            (message["text"], time["text"]) for message, time in message_labels
-        ]
-
-        for message in messages:
-            message_time = datetime.datetime.fromtimestamp(message["date"]).strftime(
-                "%d-%m-%Y %H:%M:%S"
-            )
-            if (message["message"], message_time) not in existing_labels:
-                self._create_message_container(message["message"], message_time, message_labels)
-
-    def _create_message_container(self, message_content, message_time, message_labels):
-        container = ttk.Frame(self.messages_frame)
-        container.columnconfigure(1, weight=1)
-        container.grid(sticky="EW", padx=(10, 50), pady=10)
-
-        self._create_message_bubble(container, message_content, message_time, message_labels)
-    
-    def _create_message_bubble(self, container, message_content, message_time, message_labels):
-        avatar_image = Image.open("./assets/male.png")
-        avatar_photo = ImageTk.PhotoImage(avatar_image)
-
-        avatar_label = ttk.Label(
-            container,
-            image=avatar_photo,
-        )
-        avatar_label.image = avatar_photo
-        avatar_label.grid(
-            row=0,
-            column=0,
-            rowspan=2,
-            sticky="NEW",
-            padx=(0, 10),
-            pady=(5, 0),
-        )
-
-        time_label = ttk.Label(container, text=message_time)
-        time_label.grid(row=0, column=1, sticky="NEW")
-
-        message_label = ttk.Label(
-            container,
-            text=message_content,
-            anchor="w",
-            justify="left",
-        )
-
-        message_label.grid(row=1, column=1, sticky="NSEW")
-
-        message_labels.append((message_label, time_label))
